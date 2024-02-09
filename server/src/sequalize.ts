3da40c7ae6301265d52b75a5b2e25241f0009dbb
Alan <alanpramil7@gmail.com>
2024-02-09 11:24:46 +0530

    .

--- server/src/sequalize.ts
+++ server/src/sequalize.ts
@@ -3,7 +3,7 @@ import { Sequelize } from "sequelize";
 require("dotenv").config();
 
 const sequelize = new Sequelize(
-  process.env.DB_DATABASE,
+  process.env.DB_NAME,
   process.env.DB_USERNAME,
   process.env.DB_PASSWORD,
   {
